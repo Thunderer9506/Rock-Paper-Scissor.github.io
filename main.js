7cf511187eb16c5b7ef2bbdbb57c70ef40498d04
Shaurya Srivastava <srivastavashaurya225@gmail.com>
2024-04-14 21:32:53 +0530

    completed the rules section

---
+++ main.js
@@ -0,0 +1,10 @@
+const close = document.getElementById("rulesImage")
+const rule = document.getElementById("rule")
+
+function closeButton(){
+    close.style.display = "none"
+}
+
+function ruleButton(){
+    close.style.display = "flex"
+}
