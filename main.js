64a0e209bf7ba5539e72e00dd16e893545f4aa26
Shaurya Srivastava <srivastavashaurya225@gmail.com>
2024-04-15 21:20:22 +0530

    house choose

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const close = document.getElementById("rulesImage")
 const buttons = document.querySelectorAll(".button")
 const user = document.querySelector(".bg_user")
-// document.querySelector(".bg_house button").style.display = "none"
-// document.querySelector(".houseButton .bg_house").classList.remove("bg_house")
+const house = document.querySelector(".bg_house")
+const choose = ["rock","paper","scissor"];
+
+let random = Math.floor(Math.random() * choose.length);
+console.log(random, choose[random]);
 
 
 function closeButton(){
@@ -12,32 +15,55 @@ function closeButton(){
 function ruleButton(){
     close.style.display = "flex"
 }
+function houseButtons(choice){
+    if (choice == "rock"){
+        house.style.backgroundColor = "hsl(349, 70%, 56%)"
+        house.style.boxShadow = "0px 7px 0px hsl(349, 71%, 50%)"
+        document.querySelector(".bg_house img").src = "images/icon-rock.svg"
+        document.querySelector(".bg_house button").value = "rock"
+    }
+    else if (choice == "scissor"){
+        house.style.backgroundColor = "hsl(40, 84%, 53%)"
+        house.style.boxShadow = "0px 7px 0px hsl(39, 89%, 47%)"
+        document.querySelector(".bg_house img").src = "images/icon-scissors.svg"
+        document.querySelector(".bg_house button").value = "scissor"
+    }
+    else if (choice == "paper"){
+        house.style.backgroundColor = "hsl(230, 89%, 65%)"
+        house.style.boxShadow = "0px 7px 0px hsl(230, 89%, 60%)"
+        document.querySelector(".bg_house img").src = "images/icon-paper.svg"
+        document.querySelector(".bg_house button").value = "paper"
+    }
+}
 
-function playButtons(event){
-    console.log(this.value)
+
+
+function userButtons(event){
     document.querySelector(".play").style.display = "none"
     document.querySelector(".result").style.display = "flex"
-    document.querySelector(".emptyBackground button").style.display = "none"
     if (this.value == "rock"){
         user.style.backgroundColor = "hsl(349, 70%, 56%)"
         user.style.boxShadow = "0px 7px 0px hsl(349, 71%, 50%)"
         document.querySelector(".bg_user img").src = "images/icon-rock.svg"
+        document.querySelector(".bg_user button").value = "rock"
     }
     else if (this.value == "scissor"){
         user.style.backgroundColor = "hsl(40, 84%, 53%)"
         user.style.boxShadow = "0px 7px 0px hsl(39, 89%, 47%)"
         document.querySelector(".bg_user img").src = "images/icon-scissors.svg"
+        document.querySelector(".bg_user button").value = "scissor"
     }
     else if (this.value == "paper"){
         user.style.backgroundColor = "hsl(230, 89%, 65%)"
         user.style.boxShadow = "0px 7px 0px hsl(230, 89%, 60%)"
         document.querySelector(".bg_user img").src = "images/icon-paper.svg"
+        document.querySelector(".bg_user button").value = "paper"
     }
-    
+    houseButtons(choose[random])
 }
 
 for (let i=0;i<buttons.length;i++){
-    buttons[i].addEventListener("click",playButtons)
+    buttons[i].addEventListener("click",userButtons)
 }
 
 
