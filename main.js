38d8cc171774d16b91b8d76b8f8c92a31b9b3b89
Shaurya Srivastava <srivastavashaurya225@gmail.com>
2024-04-16 11:30:48 +0530

    Completed the Logic part

--- main.js
+++ main.js
@@ -7,9 +7,8 @@ let score = 0
 let player1 = ""
 let player2 = ""
 
-let random = Math.floor(Math.random() * choose.length);
-console.log(choose[random]);
-
+let random
+choosefunction()
 
 function closeButton(){
     close.style.display = "none"
@@ -19,13 +18,30 @@ function ruleButton(){
     close.style.display = "flex"
 }
 
+function choosefunction(){
+    random = Math.floor(Math.random() * choose.length);
+    console.log(choose[random]);
+}
+
+function playagainButton(){
+    document.querySelector(".play").style.display = "flex"
+    document.querySelector(".result").style.display = "none"
+    choosefunction()
+}
+
+
 function winLose(what){
     if (what == "win"){
         document.querySelector(".winLose p").innerText = "YOU WIN"
         document.querySelector(".playAgain").style.color = "hsl(237, 49%, 15%)"
     }
+    else if (what == "lose"){
+        document.querySelector(".winLose p").innerText = "YOU LOSE"
+        document.querySelector(".playAgain").style.color = "hsl(349, 71%, 52%)"
+    }
     else if (what == "tie"){
         document.querySelector(".winLose p").innerText = "IT'S A TIE"
+        document.querySelector(".playAgain").style.color = "hsl(349, 71%, 52%)"
     }
 }
 
@@ -39,8 +55,8 @@ function engine(){
         if (player2 == "paper"){
             if (score != 0){
                 score  = score - 1
-                record("lose")
             }
+            record("lose")
         }
         else if (player2 == "scissor"){
             score  = score + 1
@@ -54,8 +70,8 @@ function engine(){
         if (player2 == "scissor"){
             if (score != 0){
                 score  = score - 1
-                record("lose")
             }
+            record("lose")
         }
         else if (player2 == "rock"){
             score  = score + 1
@@ -69,8 +85,8 @@ function engine(){
         if (player2 == "rock"){
             if (score != 0){
                 score  = score - 1
-                record("lose")
             }
+            record("lose")
         }
         else if (player2 == "paper"){
             score  = score + 1
@@ -82,26 +98,31 @@ function engine(){
     }
 }
 
-function houseButtons(choice){
-    player2 = choice
+function button(choice,where,which){
     if (choice == "rock"){
-        house.style.backgroundColor = "hsl(349, 70%, 56%)"
-        house.style.boxShadow = "0px 7px 0px hsl(349, 71%, 50%)"
-        document.querySelector(".bg_house img").src = "images/icon-rock.svg"
-        document.querySelector(".bg_house button").value = "rock"
+        which.style.backgroundColor = "hsl(349, 70%, 56%)"
+        which.style.boxShadow = "0px 7px 0px hsl(349, 71%, 50%)"
+        where.concat(" button")
+        document.querySelector(where.concat(" img")).src = "images/icon-rock.svg"
+        document.querySelector(where.concat(" button")).value = "rock"
     }
     else if (choice == "scissor"){
-        house.style.backgroundColor = "hsl(40, 84%, 53%)"
-        house.style.boxShadow = "0px 7px 0px hsl(39, 89%, 47%)"
-        document.querySelector(".bg_house img").src = "images/icon-scissors.svg"
-        document.querySelector(".bg_house button").value = "scissor"
+        which.style.backgroundColor = "hsl(40, 84%, 53%)"
+        which.style.boxShadow = "0px 7px 0px hsl(39, 89%, 47%)"
+        document.querySelector(where.concat(" img")).src = "images/icon-scissors.svg"
+        document.querySelector(where.concat(" button")).value = "scissor"
     }
     else if (choice == "paper"){
-        house.style.backgroundColor = "hsl(230, 89%, 65%)"
-        house.style.boxShadow = "0px 7px 0px hsl(230, 89%, 60%)"
-        document.querySelector(".bg_house img").src = "images/icon-paper.svg"
-        document.querySelector(".bg_house button").value = "paper"
+        which.style.backgroundColor = "hsl(230, 89%, 65%)"
+        which.style.boxShadow = "0px 7px 0px hsl(230, 89%, 60%)"
+        document.querySelector(where.concat(" img")).src = "images/icon-paper.svg"
+        document.querySelector(where.concat(" button")).value = "paper"
     }
+}
+
+function houseButtons(choice){
+    player2 = choice
+    button(choice,".bg_house",house)
     engine()
 }
 
@@ -109,28 +130,11 @@ function userButtons(event){
     player1 = this.value
     document.querySelector(".play").style.display = "none"
     document.querySelector(".result").style.display = "flex"
-    if (this.value == "rock"){
-        user.style.backgroundColor = "hsl(349, 70%, 56%)"
-        user.style.boxShadow = "0px 7px 0px hsl(349, 71%, 50%)"
-        document.querySelector(".bg_user img").src = "images/icon-rock.svg"
-        document.querySelector(".bg_user button").value = "rock"
-    }
-    else if (this.value == "scissor"){
-        user.style.backgroundColor = "hsl(40, 84%, 53%)"
-        user.style.boxShadow = "0px 7px 0px hsl(39, 89%, 47%)"
-        document.querySelector(".bg_user img").src = "images/icon-scissors.svg"
-        document.querySelector(".bg_user button").value = "scissor"
-    }
-    else if (this.value == "paper"){
-        user.style.backgroundColor = "hsl(230, 89%, 65%)"
-        user.style.boxShadow = "0px 7px 0px hsl(230, 89%, 60%)"
-        document.querySelector(".bg_user img").src = "images/icon-paper.svg"
-        document.querySelector(".bg_user button").value = "paper"
-    }
+    button(this.value,".bg_user",user)
     houseButtons(choose[random])
 }
 
-for (let i=0;i<buttons.length;i++){
+for (let i=2;i<buttons.length;i++){
     buttons[i].addEventListener("click",userButtons)
 }
 
